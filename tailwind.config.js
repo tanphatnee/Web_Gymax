671734071da5ac5d5632d0a7aabefe4104525469
tanphattai37 <nvtphat2k4@gmail.com>
2022-12-26 10:52:34 +0700

    .

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  mode: 'jit',
+  purge: [
+    "**/*.html"
+  ],
+  theme: {
+    screens:{
+      '1170':'1170px',
+      '1024':'1024',
+      '768':'768px',
+    }
+  },
+  plugins: [],
+}
